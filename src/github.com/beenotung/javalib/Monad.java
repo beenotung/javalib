c44b3e5ccf4b70aafbdffe5aa3c8dac35208bbe5
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-09-02 22:18:02 -0400

    added .editorconfig and .gitignore

---
+++ src/github.com/beenotung/javalib/Monad.java
@@ -0,0 +1,5 @@
+package github.com.beenotung.javalib;
+
+public class Monad{
+
+}
---
+++ src/github.com/beenotung/javalib/Utils.java
@@ -0,0 +1,11 @@
+package github.com.beenotung.javalib;
+
+public class Utils {
+  public static void print(Object msg) {
+    System.out.print(msg);
+  }
+
+  public static void println(Object msg) {
+    System.out.println(msg);
+  }
+}
