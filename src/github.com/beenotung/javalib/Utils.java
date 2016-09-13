79c9fbbfb7b048c7c704de4bc65b70bbaca17599
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-09-13 19:05:15 -0400

    added Pair3

--- src/github.com/beenotung/javalib/Utils.java
+++ src/github.com/beenotung/javalib/Utils.java
@@ -386,6 +386,38 @@ public class Utils {
     }
   }
 
+  public static class Pair3<T1, T2, T3> {
+    public final T1 _1;
+    public final T2 _2;
+    public final T3 _3;
+
+    public Pair3(T1 t1, T2 t2, T3 t3) {
+      _1 = t1;
+      _2 = t2;
+      _3 = t3;
+    }
+
+    /**
+     * @param idx indicate which element to drop, start from 1 , [1,3]
+     */
+    public <A, B> Pair<A, B> truncate(int idx) {
+      if (idx == 1)
+        return pair((A) _2, (B) _3);
+      else if (idx == 2)
+        return pair((A) _1, (B) _3);
+      else
+        return pair((A) _1, (B) _2);
+    }
+
+    public <A, B> Pair<A, B> truncate() {
+      return truncate(3);
+    }
+  }
+
+  public static <T1, T2, T3> Pair3<T1, T2, T3> pair3(T1 _1, T2 _2, T3 _3) {
+    return new Pair3<T1, T2, T3>(_1, _2, _3);
+  }
+
   public static class Either<A, B> extends Pair<A, B> {
     public final boolean isLeft;
     public final boolean isRight;
